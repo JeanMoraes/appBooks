57b496d3a50406afc805c5648b94d97255955001
Stefanny <stefannylopes@gmail.com>
2020-01-30 15:03:41 -0300

    construção do componente header

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import "./Header.css";
+
+const Header = () => {
+    return(
+        <header>
+            <i className="fas fa-book"></i>
+            <h1>Google<strong>Books</strong></h1>
+        </header>
+    )
+}
+
+export default Header;
